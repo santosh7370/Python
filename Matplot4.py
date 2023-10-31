aa181687425354a812bb231bf9964b154fdd82ed
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2023-10-31 23:55:47 +0530

    Add files via upload

---
+++ Matplot4.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt  
+  
+x = [5, 2, 7]  
+y = [1, 10, 4]  
+plt.plot(x, y)  
+plt.title('Line graph')  
+plt.ylabel('Y axis')  
+plt.xlabel('X axis')  
+plt.show()  
---
+++ Matplot5.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt  
+plt.plot([1,2,3,4,5],[1,4,9,16,25])  
+plt.ylabel("y axis")  
+plt.xlabel('x axis')  
+plt.show()  
